86dd6c422797218449c15e03f93005366d050fb7
Devesh Paragiri <deveshparagiri@Deveshs-MacBook-Pro.local>
2023-12-28 10:22:25 +0530

    Hushh Jobs v1

---
+++ embeddings.py
@@ -0,0 +1,15 @@
+import requests
+
+model_id = "sentence-transformers/all-MiniLM-L6-v2"
+
+api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+headers = {"Authorization": f"Bearer "}
+
+
+def text_embedding(texts):
+    response = requests.post(
+        api_url,
+        headers=headers,
+        json={"inputs": texts, "options": {"wait_for_model": True}},
+    )
+    return response.json()
