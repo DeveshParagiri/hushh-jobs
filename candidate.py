42e53ecfd79ff51f33e55f72c4b3f9e60580815d
Vasundhhara Katoch <katoch@Vasundhharas-MacBook-Air.local>
2024-01-06 00:30:06 +0530

    add phone number field

--- candidate.py
+++ candidate.py
@@ -5,6 +5,7 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 class Candidate(BaseModel):
     name: str = Field(description="First name and last name of the candidate.")
     email: str = Field(description="Email address of the candidate.")
+    phone: str = Field(description="Contact number with country code of the candidate.")
     location: str = Field(description="City and state where the candidate resides.")
     degree: List[str] = Field(description="List of the candidate's college degrees.")
     college: List[str] = Field(description="List of all the colleges candidate went to")
