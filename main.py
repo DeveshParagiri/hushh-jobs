8896f7d627e313c3b45274db8cf882a000b6b0a1
Vasundhhara <vsk.isha16@gmail.com>
2024-01-08 03:36:40 +0530

    update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def main():
             field_names = [
                 "name",
                 "email",
-                "phone"
+                "phone",
                 "location",
                 "degree",
                 "college",
